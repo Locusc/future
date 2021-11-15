b734ac4666e132b69a21d06e1a311745017c2f9c
locusc <2227628925@qq.com>
2021-11-15 10:30:33 +0800

    init code

---
+++ future-java-explore/future-jdk-explore/src/main/java/cn/locusc/future/jdk/explore/version8/StreamExplore.java
@@ -0,0 +1,15 @@
+package cn.locusc.future.jdk.explore.version8;
+
+import java.util.stream.Stream;
+
+/**
+ * @author Jay
+ * Stream
+ * 2021/11/12
+ */
+public class StreamExplore {
+
+    public static void main(String[] args) {
+
+    }
+}
