096159b27d08a1e813f8c6a17a75d6273eafb12a
locusc <2227628925@qq.com>
2021-11-17 10:05:51 +0800

    add java in action chapter II

---
+++ future-book-explore/future-java8-in-action-explore/src/main/java/cn/locusc/java8action/chapterI/FunctionalProgramming15.java
@@ -0,0 +1,17 @@
+package cn.locusc.java8action.chapterI;
+
+import java.util.Optional;
+
+/**
+ * @author Jay
+ * 来自函数式编程的其他好思想
+ * 2021/11/16
+ */
+public class FunctionalProgramming15 {
+
+    public static void main(String[] args) {
+        String test = "";
+        Optional.of(test).orElse(null);
+    }
+
+}
