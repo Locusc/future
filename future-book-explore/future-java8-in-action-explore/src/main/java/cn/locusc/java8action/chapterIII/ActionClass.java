c79f0064de3f708cc7a85f9017cb32de9e77b8db
locusc <2227628925@qq.com>
2021-11-18 10:03:46 +0800

    add java in action chapter III

--- future-book-explore/future-java8-in-action-explore/src/main/java/cn/locusc/java8action/chapterIII/ActionClass.java
+++ future-book-explore/future-java8-in-action-explore/src/main/java/cn/locusc/java8action/chapterIII/ActionClass.java
@@ -1,8 +1,8 @@
 package cn.locusc.java8action.chapterIII;
 
-import cn.locusc.java8action.chapterII.PracticeClass;
 import cn.locusc.java8action.domain.Apple;
 
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -10,12 +10,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.IntPredicate;
-import java.util.function.Predicate;
-
-import static cn.locusc.java8action.chapterII.PracticeClass.filter;
+import java.util.function.*;
 
 /**
  * @author Jay
@@ -79,6 +74,8 @@ public class ActionClass {
         return result;
     }
 
+    public static String testString = "testString";
+
     public static void main(String[] args) throws IOException {
         List<Apple> inventory = Apple.getApples();
 
@@ -136,6 +133,60 @@ public class ActionClass {
         // Function
         // 需要定义一个Lambda，将输入对象的信息映射到输出，就可以使用这个接口
         map(strings, (String s) -> s.length());
+
+        // 显式捕获异常
+        Function<BufferedReader, String> f = (BufferedReader b) -> {
+            try {
+                return b.readLine();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        };
+
+        // 同样的 Lambda，不同的函数式接口
+        Comparator<Apple> c1 = (Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight());
+        ToIntBiFunction<Apple, Apple> c2 = (Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight());
+        BiFunction<Apple, Apple, Integer> c3 = (Apple a1, Apple a2) -> a1.getWeight().compareTo(a2.getWeight());
+
+        // 个Lambda的主体是一个语句表达式，它就和一个返回void的函数描述符兼容（当然需要参数列表也兼容）
+        Predicate<String> p = s -> strings.add(s);
+        Consumer<String> b = s -> strings.add(s);
+
+        // Object o = () -> {System.out.println("Tricky example"); };
+        // Lambda表达式的上下文是Object（目标类型）。但Object不是一个函数式接口。
+        // 为了解决这个问题，你可以把目标类型改成Runnable，它的函数描述符是() -> void：
+        Runnable r = () -> {System.out.println("Tricky example");};
+
+        // 类型推断
+        // Java编译器会从上下文（目标类型）推断出用什么函数式接
+        // 口来配合Lambda表达式，这意味着它也可以推断出适合Lambda的签名，因为函数描述符可以通
+        // 过目标类型来得到。这样做的好处在于，编译器可以了解Lambda表达式的参数类型，这样就可
+        // 以在Lambda语法中省去标注参数类型。换句话说，Java编译器会像下面这样推断Lambda的参数
+        // 类型：①
+        filter(inventory, a -> "green".equals(a.getColor()));
+
+        // 使用局部变量 实例变量都存储在堆中 局部变量则保存在栈上
+        // 如果Lambda可以直接访问局部变量，而且Lambda是在一个线程中使用的，则使用Lambda的线程，可能会在分配该变量的线程将这个变量收回之后，去访问该变量
+        // Java在访问自由局部变量时，实际上是在访问它的副本，而不是访问原始变量。
+        // 局部变量必须隐式或者显式final修饰 避免线程问题
+        // 这里会提示错误 Lambda表达式引用的局部变量必须是最终的（final）或事实上最终的
+        int portNumber = 1337;
+        // Runnable runnable = () -> System.out.println(portNumber);
+        portNumber = 31337;
+
+    }
+
+
+    // 方法引用
+    public static class MethodReference {
+
+        public static void main(String[] args) {
+
+            List<Apple> inventory = Apple.getApples();
+
+
+        }
+
     }
 
 }
