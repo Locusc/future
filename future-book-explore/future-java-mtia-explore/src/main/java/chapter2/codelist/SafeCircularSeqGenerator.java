940b1ceb8f27a772c5b788fe349d1f5474ff5179
locusc <2227628925@qq.com>
2022-06-16 11:18:04 +0800

    change future-java-mtia-explore

--- future-book-explore/future-java-mtia-explore/src/main/java/chapter2/codelist/SafeCircularSeqGenerator.java
+++ future-book-explore/future-java-mtia-explore/src/main/java/chapter2/codelist/SafeCircularSeqGenerator.java
@@ -1,9 +1,34 @@
 package chapter2.codelist;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class SafeCircularSeqGenerator implements CircularSeqGenerator21 {
 
     private short sequence = -1;
 
+    private final Lock lock = new ReentrantLock();
+
+    /**
+     * 显式锁
+     */
+    public short nextSequenceExplicitLock() {
+        lock.lock();
+        try {
+            if (sequence >= 999) {
+               sequence = 0;
+            } else {
+               sequence++;
+            }
+            return sequence;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    /**
+     * 内部锁
+     */
     @Override
     public synchronized short nextSequence() {
         if (sequence >= 999) {
@@ -12,6 +37,29 @@ public class SafeCircularSeqGenerator implements CircularSeqGenerator21 {
             sequence++;
         }
         return sequence;
+    }
+
+    // 相当于
+    public short nextSequenceBlock() {
+        synchronized (this) {
+            if (sequence >= 999) {
+                sequence = 0;
+            } else {
+                sequence++;
+            }
+            return sequence;
+        }
+    }
+
+    public static synchronized void staticMethod() {
+        // 在此访问共享数据
+    }
+
+    // 相当于
+    public static void staticMethodBlock() {
+        synchronized (SafeCircularSeqGenerator.class) {
+            // ...
+        }
     }
 
 }
