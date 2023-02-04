18d1ebca0e9494495e606d536cea962430e88333
locusc <2227628925@qq.com>
2023-02-04 10:56:55 +0800

    change future-deep-understanding-of-jvm-explore

---
+++ future-book-explore/future-deep-understanding-of-jvm-explore/src/main/java/cn/locusc/duo/jvm/part2/chapter3/codelist/TestSerialGCAllocation33.java
@@ -0,0 +1,83 @@
+package cn.locusc.duo.jvm.part2.chapter3.codelist;
+
+/**
+ * @author Jay
+ * VM参数：+XX:UseSerialGC
+ * 内存分配与回收策略
+ * 2022/12/29
+ */
+public class TestSerialGCAllocation33 {
+
+    private static final int _1MB = 1024 * 1024;
+
+    /**
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+     * page:184 对象优先在Eden分配
+     */
+    public static void testAllocation() {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        allocation4 = new byte[4 * _1MB];  // 出现一次Minor GC
+    }
+
+    /**
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8
+     * -XX:PretenureSizeThreshold=3145728
+     * page:186 大对象直接进入老年代
+     */
+    public static void testPretenureSizeThreshold() {
+        byte[] allocation;
+        allocation = new byte[4 * _1MB];  //直接分配在老年代中
+    }
+
+    /**
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=1
+     * -XX:+PrintTenuringDistribution
+     * page:187 长期存活的对象将进入老年代
+     */
+    public static void testTenuringThreshold() {
+        byte[] allocation1, allocation2, allocation3;
+        allocation1 = new byte[_1MB / 4];  // 什么时候进入老年代决定于XX:MaxTenuringThreshold设置
+        allocation2 = new byte[4 * _1MB];
+        allocation3 = new byte[4 * _1MB];
+        allocation3 = null;
+        allocation3 = new byte[4 * _1MB];
+    }
+
+    /**
+     * VM参数：-verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:MaxTenuringThreshold=15
+     * -XX:+PrintTenuringDistribution
+     * page:189 动态对象年龄判定
+     */
+    public static void testTenuringThreshold2() {
+        byte[] allocation1, allocation2, allocation3, allocation4;
+        allocation1 = new byte[_1MB / 4];   // allocation1+allocation2大于survivo空间一半
+        allocation2 = new byte[_1MB / 4];
+        allocation3 = new byte[4 * _1MB];
+        allocation4 = new byte[4 * _1MB];
+        allocation4 = null;
+        allocation4 = new byte[4 * _1MB];
+    }
+
+    /**
+     * VM参数：-Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8 -XX:-HandlePromotionFailure
+     * page:190 空间担保分配
+     */
+    public static void testHandlePromotion() {
+        byte[] allocation1, allocation2, allocation3, allocation4, allocation5, allocation6, allocation7;
+        allocation1 = new byte[2 * _1MB];
+        allocation2 = new byte[2 * _1MB];
+        allocation3 = new byte[2 * _1MB];
+        allocation1 = null;
+        allocation4 = new byte[2 * _1MB];
+        allocation5 = new byte[2 * _1MB];
+        allocation6 = new byte[2 * _1MB];
+        allocation4 = null;
+        allocation5 = null;
+        allocation6 = null;
+        allocation7 = new byte[2 * _1MB];
+    }
+
+}
