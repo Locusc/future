31b67e538e8a8a6b4bc660dbe82280517597865b
locusc <2227628925@qq.com>
2023-03-04 13:44:18 +0800

    change future-deep-understanding-of-jvm-explore

--- future-book-explore/future-deep-understanding-of-jvm-explore/src/main/java/cn/locusc/duo/jvm/part4/chapter3/codelist/JHSDBTestCase46.java
+++ future-book-explore/future-deep-understanding-of-jvm-explore/src/main/java/cn/locusc/duo/jvm/part4/chapter3/codelist/JHSDBTestCase46.java
@@ -3,6 +3,9 @@ package cn.locusc.duo.jvm.part4.chapter3.codelist;
 /**
  * @author Jay
  * staticObj, instanceObj, localObj 存放位置
+ * 64操作系统需要禁止压缩指针, 有可能在使用jhsdb时无法对对象进行寻址操作
+ * -Xmx10m -XX:+UseSerialGC -XX:-UseCompressedOops
+ * java -cp your_javahome_path/lib/sa-jdi.jar sun.jvm.hotspot.HSDB
  * 2023/1/9
  */
 public class JHSDBTestCase46 {
